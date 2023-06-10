74893afe595465c4fa8c98124bc2eef722f7d2a8
pjm2010 <prithvijit20@gmail.com>
2023-06-10 10:36:58 +0530

    Update streamlit_app.py
    added icons and made some word edits

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,10 @@
 import streamlit
 
-streamlit.title('My parents new healthy diner')
+streamlit.title('My mom''s new healthy diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 and Blueberry Oatmeal')
-streamlit.text('Kale ,spinach and rocket smoothie')
-streamlit.text('Hard Boiled Free - Range Egg')
+streamlit.header('Breakfast Favorites')
+streamlit.text(' 🥣 Omega 3 and Blueberry Oatmeal')
+streamlit.text(' 🥗 Kale ,spinach and rocket smoothie')
+streamlit.text(' 🐔 Hard Boiled Free - Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+

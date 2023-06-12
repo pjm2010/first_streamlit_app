9c36cd8e220d5451e846364e058f00402f91fbaf
pjm2010 <prithvijit20@gmail.com>
2023-06-12 19:02:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,24 @@
 import streamlit
 import pandas
 import requests
-
+import snowflake.connector
 
 streamlit.title('My mom''s new healthy diner')
-
 streamlit.header('Breakfast Favorites')
 streamlit.text(' 🥣 Omega 3 and Blueberry Oatmeal')
 streamlit.text(' 🥗 Kale ,spinach and rocket smoothie')
 streamlit.text(' 🐔 Hard Boiled Free - Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
-
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-
-
-
 my_fruit_list = my_fruit_list.set_index('Fruit')
+
+
+
+
 fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
@@ -36,7 +38,7 @@ streamlit.dataframe(fruityvice_normalized)
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-import snowflake.connector
+
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
@@ -47,4 +49,5 @@ streamlit.dataframe(my_data_row)
 
 second_choice = streamlit.text_input('What fruit would you like information about?','jackfruit')
 streamlit.write('Thanks for adding', second_choice)
+mycur.exec("insert into food_load_list values ('from streamlit')");
 
